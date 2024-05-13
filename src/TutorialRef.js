458a4a53ff5bcc3740cc040937d90f0f583d657e
QBEAU\231403 <jennifer.devega@qbe.com>
2024-05-13 16:15:58 +1000

    Added new component for useRef

---
+++ src/TutorialRef.js
@@ -0,0 +1,25 @@
+import React, {useRef} from "react";
+
+const TutorialRef= () => {
+    const inputRef = useRef(null);
+    
+    const onClick = () => {
+        console.log(inputRef.current.value);
+    };
+
+    return(
+        <div>
+            <h1>05 useRef</h1>
+            <input 
+                type="text"
+                placeholder="Ex...."
+                ref={inputRef}
+            />
+            <button onClick={onClick}>
+                Change Name
+            </button>
+        </div>
+    );
+}
+
+export default TutorialRef;
